4a9199b124b44b6ff0a323df389f0246ce0acd1f
joeldholm <joeldholm@gmail.com>
2016-12-28 15:35:24 -0600

    Added the sendCommand() function for the 4 main buttons, and constantly updates the x and y values according to the table on the google docs.  Few other small changes

--- app/src/main/java/joelholm/testground/MainActivity.java
+++ app/src/main/java/joelholm/testground/MainActivity.java
@@ -35,20 +35,32 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     private Sensor sensor;
     private SensorManager SM;
 
-    //Stuff from engineering priject website
+    //Stuff from engineering project website
     private UsbManager usbManager;
     private UsbDevice deviceFound;
     private UsbDeviceConnection usbDeviceConnection;
     private UsbInterface usbInterfaceFound = null;
     private UsbEndpoint endpointOut = null;
     private UsbEndpoint endpointIn = null;
+    SeekBar bar;
+    ToggleButton buttonLed;
 
+    //Variables for controls
+    boolean up = false,down = false,counter = false,clock = false;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Engineering website stuff
+        usbManager = (UsbManager)getSystemService(Context.USB_SERVICE);
+
+
+
+
+
         //Pause Button setup
         ImageButton pause = (ImageButton)findViewById(R.id.pause_button);
         pause.setOnClickListener(new View.OnClickListener() {
@@ -68,16 +80,18 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             public boolean onTouch(View v, MotionEvent event) {
                 if (event.getAction() == MotionEvent.ACTION_DOWN) {
                     //Start
-                    Log.d("T","Start");
-                    sound.startTone(ToneGenerator.TONE_DTMF_3, 500);
+                    //sound.startTone(ToneGenerator.TONE_DTMF_3, 500);
+                    up = true;
 
                 } else if (event.getAction() == MotionEvent.ACTION_UP) {
                     //Stop
-                    Log.d("T","End");
+                    up = false;
                 }
                 return false;
             }
         });
+
+
         //Down button
         Button downButton = (Button)findViewById(R.id.downButton);
         downButton.setOnTouchListener(new View.OnTouchListener() {
@@ -85,10 +99,10 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             public boolean onTouch(View v, MotionEvent event) {
                 if (event.getAction() == MotionEvent.ACTION_DOWN) {
                     //Start
-                    Log.d("T","Start");
+                    down = true;
                 } else if (event.getAction() == MotionEvent.ACTION_UP) {
                     //Stop
-                    Log.d("T","End");
+                    down = false;
                 }
                 return false;
             }
@@ -100,10 +114,10 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             public boolean onTouch(View v, MotionEvent event) {
                 if (event.getAction() == MotionEvent.ACTION_DOWN) {
                     //Start
-                    Log.d("T","Start");
+                    counter = true;
                 } else if (event.getAction() == MotionEvent.ACTION_UP) {
                     //Stop
-                    Log.d("T","End");
+                    counter = false;
                 }
                 return false;
             }
@@ -115,10 +129,10 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             public boolean onTouch(View v, MotionEvent event) {
                 if (event.getAction() == MotionEvent.ACTION_DOWN) {
                     //Start
-                    Log.d("T","Start");
+                    clock = true;
                 } else if (event.getAction() == MotionEvent.ACTION_UP) {
                     //Stop
-                    Log.d("T","End");
+                    clock = false;
                 }
                 return false;
             }
@@ -151,11 +165,149 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         * event.values[1] for y
         * and event.values[2] for z
         */
+        //Testing x accelerometer
+        //Log.d("T", String.valueOf(event.values[0]));
 
+        int x,y;
+        x = 30 + (int)(event.values[0] * 20);
+        y = 71 + (int)(event.values[1] * 20);
+
+        sendCommand(x);
+        sendCommand(y);
+
+        if ( up == true ){
+            Log.d("T","up");
+            sendCommand(1);
+        }
+        if ( down == true ){
+            Log.d("T","down");
+            sendCommand(2);
+        }
+        if ( counter == true ){
+            Log.d("T","counter");
+            sendCommand(4);
+        }
+        if ( clock == true ){
+            Log.d("T","clock");
+            sendCommand(3);
+        }
 
 
 
     }
 
+
+
+
+    //Functions from http://www.theengineeringprojects.com/2015/10/usb-communication-between-android-and-arduino.html
+    //Then use sendCommand(' '); to send a char value, or int, idk, to an arduino board.
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        Intent intent = getIntent();
+        String action = intent.getAction();
+
+        UsbDevice device = (UsbDevice)intent.getParcelableExtra(UsbManager.EXTRA_DEVICE);
+        if (UsbManager.ACTION_USB_DEVICE_ATTACHED.equals(action)) {
+            setDevice(device);
+        } else if (UsbManager.ACTION_USB_DEVICE_DETACHED.equals(action)) {
+            if (deviceFound != null && deviceFound.equals(device)) {
+                setDevice(null);
+            }
+        }
+    }
+
+    private void setDevice(UsbDevice device) {
+        usbInterfaceFound = null;
+        endpointOut = null;
+        endpointIn = null;
+
+        for (int i = 0; i < device.getInterfaceCount(); i++) {
+            UsbInterface usbif = device.getInterface(i);
+
+            UsbEndpoint tOut = null;
+            UsbEndpoint tIn = null;
+
+            int tEndpointCnt = usbif.getEndpointCount();
+            if (tEndpointCnt >= 2) {
+                for (int j = 0; j < tEndpointCnt; j++) {
+                    if (usbif.getEndpoint(j).getType() == UsbConstants.USB_ENDPOINT_XFER_BULK) {
+                        if (usbif.getEndpoint(j).getDirection() == UsbConstants.USB_DIR_OUT) {
+                            tOut = usbif.getEndpoint(j);
+                        } else if (usbif.getEndpoint(j).getDirection() == UsbConstants.USB_DIR_IN) {
+                            tIn = usbif.getEndpoint(j);
+                        }
+                    }
+                }
+
+                if (tOut != null && tIn != null) {
+                    // This interface have both USB_DIR_OUT
+                    // and USB_DIR_IN of USB_ENDPOINT_XFER_BULK
+                    usbInterfaceFound = usbif;
+                    endpointOut = tOut;
+                    endpointIn = tIn;
+                }
+            }
+
+        }
+
+        if (usbInterfaceFound == null) {
+            return;
+        }
+
+        deviceFound = device;
+
+        if (device != null) {
+            UsbDeviceConnection connection =
+                    usbManager.openDevice(device);
+            if (connection != null &&
+                    connection.claimInterface(usbInterfaceFound, true)) {
+                usbDeviceConnection = connection;
+                Thread thread = new Thread();
+                thread.start();
+
+            } else {
+                usbDeviceConnection = null;
+            }
+        }
+    }
+
+    private void sendCommand(int control) {
+        synchronized (this) {
+
+            if (usbDeviceConnection != null) {
+                byte[] message = new byte[1];
+                message[0] = (byte)control;
+                usbDeviceConnection.bulkTransfer(endpointOut,
+                        message, message.length, 0);
+            }
+        }
+    }
+
+    //@Override
+    public void run() {
+        ByteBuffer buffer = ByteBuffer.allocate(1);
+        UsbRequest request = new UsbRequest();
+        request.initialize(usbDeviceConnection, endpointIn);
+        while (true) {
+            request.queue(buffer, 1);
+            if (usbDeviceConnection.requestWait() == request) {
+                byte rxCmd = buffer.get(0);
+                if(rxCmd!=0){
+                    bar.setProgress((int)rxCmd);
+                }
+
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                }
+            } else {
+                break;
+            }
+        }
+
+    }
 
 }
--- app/src/main/java/joelholm/testground/Pop.java
+++ app/src/main/java/joelholm/testground/Pop.java
@@ -1,9 +1,10 @@
 package joelholm.testground;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 
@@ -29,9 +30,14 @@ public class Pop extends Activity {
         resume.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(Pop.this,MainActivity.class));
+                finish();
+                //startActivity(new Intent(Pop.this,MainActivity.class));
             }
         });
+
+
+
+
 
     }
 }
